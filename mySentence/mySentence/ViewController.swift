b49153d8a85045f0f007c5d3991646a99e44e9ba
JH713 <strider13@snu.ac.kr>
2023-11-17 00:23:12 +0900

    Init: 프로젝트 생성

---
+++ mySentence/mySentence/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mySentence
+//
+//  Created by 이지현 on 2023/11/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
